ff4be7cb108d7bdbb9dcb32bceb15f7fc1780c99
munergenis <154993411+munergenis@users.noreply.github.com>
2024-05-06 02:11:27 +0200

    click on answers toggles selected answers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { nanoid } from "nanoid"
+import { decode } from "html-entities"
 import "./App.css"
 import Question from "./components/Question"
 
@@ -14,21 +15,21 @@ export default function App() {
           data.results.map((entry) => {
             const answers = entry.incorrect_answers.map((answer) => ({
               id: nanoid(),
-              value: answer,
+              value: decode(answer),
               isCorrect: false,
               isSelected: false,
             }))
 
             answers.push({
               id: nanoid(),
-              value: entry.correct_answer,
+              value: decode(entry.correct_answer),
               isCorrect: true,
               isSelected: false,
             })
 
             return {
               id: nanoid(),
-              question: entry.question,
+              question: decode(entry.question),
               answers: answers,
             }
           })
@@ -36,8 +37,45 @@ export default function App() {
       })
   }, [])
 
+  function toggleSelected(id) {
+    const targetQuestion = questionObjects.find((question) =>
+      question.answers.some((answer) => answer.id === id)
+    )
+    const targetAnswer = targetQuestion.answers.find(
+      (answer) => answer.id === id
+    )
+
+    if (targetAnswer.isSelected) return
+
+    setQuestionObjects((prevQuestionObj) => {
+      const newQuestionObj = prevQuestionObj.map((question) => {
+        if (question.id === targetQuestion.id) {
+          return {
+            ...question,
+            answers: question.answers.map((answer) => {
+              return answer.id === id
+                ? { ...answer, isSelected: true }
+                : { ...answer, isSelected: false }
+            }),
+          }
+        } else {
+          return question
+        }
+      })
+      return newQuestionObj
+    })
+  }
+
   const questionElements = questionObjects.map(({ id, question, answers }) => {
-    return <Question key={id} id={id} question={question} answers={answers} />
+    return (
+      <Question
+        key={id}
+        id={id}
+        question={question}
+        answers={answers}
+        handleClick={toggleSelected}
+      />
+    )
   })
 
   return <section>{questionElements}</section>
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,9 +1,15 @@
-export default function Question({ id, question, answers }) {
+export default function Question({ id, question, answers, handleClick }) {
   return (
     <div>
       <h2>{question}</h2>
-      {answers.map(({ id, value }) => (
-        <p key={id}>{value}</p>
+      {answers.map(({ id, value, isSelected }) => (
+        <p
+          className={isSelected ? "selected" : undefined}
+          key={id}
+          onClick={() => handleClick(id)}
+        >
+          {value}
+        </p>
       ))}
     </div>
   )
